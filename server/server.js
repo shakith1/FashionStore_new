92b60effbc96649463c727b35ee63d179c291890
unknown <nimsara.shakith@gmail.com>
2020-06-21 00:13:57 +0530

    Production

--- server/server.js
+++ server/server.js
@@ -18,6 +18,8 @@ const { auth } = require('./middleware/auth')
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(express.static('client/build'))
+
 // GET //
 
 app.get('/api/getCategory', (req, res) => {
@@ -324,6 +326,13 @@ app.post('/products/update/:id', upload.single('productImage'), (req, res) => {
 const PaymentRouter = require('./routes/payment');
 
 app.use('/payment', PaymentRouter);
+
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
